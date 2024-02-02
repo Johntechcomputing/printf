6df6aa91cf4f210e8461c65127faebf75ba22b28
Johntechcomputing <johntechcomputing@gmail.com>
2024-02-02 11:32:35 +0300

    printf updates

--- main.h
+++ main.h
@@ -8,8 +8,15 @@
 #define BUFF_SIZE 1024
 
 /* SIZES */
-#define S_LONG 2
 #define S_SHORT 1
+#define S_LONG 2
+
+/* FLAGS */
+#define F_PLUS 2
+#define F_MINUS 1
+#define F_ZERO 4
+#define F_SPACE 16
+#define F_HASH 8
 
 /**
  * struct fmt - Struct op
